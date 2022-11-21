ba44ff844f9a07f7f76a82a88fb3a9bc6c094ca2
funwithforks <funwithforks@users.noreply.github.com>
2022-11-20 19:28:01 -0700

    Update idle.sh
    Made improvements to spelunking. I will need to add options to live edit speed and value values and make the time be based on that instead of a constant.
    Added mark and strafe skills to the midas rotation. Mark turns on for midas, turns off for kills. Strafe reduces cooldown of midas.
    Many variables are set based on my current skill levels in the game and I have to modify them manually.
    I don't like that xdotool uses my cursor to click and has to have the window active in order to do active skill rotation. I'm looking for a way to do clicks without my mouse other than putting Idle Skilling into a VM.

--- idle.sh
+++ idle.sh
@@ -8,6 +8,7 @@ GREEN='\033[0;32m'
 CUT='\033[0K'
 CURRENT_SCREEN='fight'
 
+
 focus_idle_skilling () {
     AW=$(xdotool search --name "^Idle Skilling$")
     if [ '$AW' ]; then
@@ -17,16 +18,18 @@ focus_idle_skilling () {
     fi
 }
 
+
 get_pid () {
     CURRENT_PID=$(xdotool getactivewindow getwindowpid)
 }
+
 
 check_game_running () {
     CPU_STAT=$(ps -eo %cpu,pid --sort -%cpu | grep $CURRENT_PID | awk '{ print $1 }' )
     if [ $CPU_STAT == 0 ]; then
             echo -en "\r    Game Froze. Recovery initiated.${CUT}"
             pkill TERM $CURRENT_PID
-            steam steam://rungameid/1048370
+            steam  steam://rungameid/1048370
             sleep 10
             focus_idle_skilling
             get_pid
@@ -34,9 +37,11 @@ check_game_running () {
             xdotool mousemove 2314 1333
             xdotool click 1
             sleep 1.5
+            read -p "Temporary safety feature... Press any key to continue"
             return
     fi
 }
+
 
 switch_modes () {
     # takes start and destination as args, then click to destination
@@ -55,16 +60,18 @@ switch_modes () {
     fi
 }
 
+
 tunnel_extract_farm () {
     # most of these star numbers are estimates
     star0=24
-    star1=190
-    star2=2080
-    star3=20000
-    speed=$((24100/60/60))
+    star1=181
+    star2=820
+    star3=8200 # was 20000, I believe this goes down as value goes up
+    star4=82000
+    speed=$((550000))
     tcounter=0
     stars=0
-    extract_secs=$(($star3/$speed))
+    extract_secs=$(($star2/($speed/60/60)))
 
     xdotool mousemove 1719 992  # get spelunker A
     xdotool click 1
@@ -81,12 +88,12 @@ tunnel_extract_farm () {
     do
         echo -en "\r   ${ORANGE}Tunnel Extract  ${RED}Jumping...${NC}${CUT}"
         focus_idle_skilling
-        sleep 2.4
+        sleep 2.4 # wait for animation
         # assuming jump in progress, can cause loss of learning or dig progress.
         # extract/jump location
         xdotool mousemove 2493 1267
         xdotool click 1 # jump
-        sleep 30
+        sleep $(($extract_secs+1))
         xdotool click 1 # extract
         # open the bag
         xdotool mousemove 1898 1336
@@ -94,21 +101,58 @@ tunnel_extract_farm () {
     done
 }
 
+
+toggle_top_skill () {
+    xdotool mousemove 1843 1302
+    xdotool click 1
+}
+
+
+toggle_bottom_skill () {
+    xdotool mousemove 1812 1366             # location of other skills, assumes on correct page
+    xdotool click 1
+}
+
+
+toggle_mark () {
+    toggle_bottom_skill
+    xdotool key 1
+
+    if [ ! -z $1 ]; then                 # strafe while you're at it
+            xdotool mousemove 1999 1311  # strafe has no key
+            xdotool click 1
+    fi
+
+    toggle_top_skill
+}
+
+
 midas_clicks () {
-    midas_secs=37
+    midas_secs=34
+    strafe=1
+    mark=1
+
     while [ $counter -le 10 ]
     do
         check_game_running
 
         echo -en "\r   ${ORANGE}Midas ${RED}Running...${NC}${CUT}"
-    
+
         focus_idle_skilling
 
-        xdotool key 3
-        xdotool mousemove 2084 1180
-        xdotool click --repeat 675 --delay 9 1
+        if [ $mark == 1 ]; then         # turn mark on
+            toggle_mark
+        fi
+
+        xdotool key 3                                   # activate Midas
+        xdotool mousemove 2084 1180                     # move mouse to empy area
+        xdotool click --repeat 670 --delay 9 1          # click a lot
         # Toggle skills page to fight skills
-        xdotool mousemove 1843 1302; xdotool click 1
+        toggle_top_skill
+
+        if [ $mark == 1 ]; then
+            toggle_mark '1'
+        fi
 
         secs=$((midas_secs))
         while [ $secs -gt 0 ]
@@ -120,27 +164,26 @@ midas_clicks () {
             read -n 1 -t 1 -p "Enter Option to do something else: "$'\r' opto
             if [ "$opto" == 'r' ]; then
                     # return skills to support skills
-                    xdotool mousemove 1843 1302; xdotool click 1
                     break
                 elif [ "$opto" == 'q' ]; then
-                    echo ''
+                    toggle_top_skill
                     exit
                 elif [ "$opto" == 'e' ]; then
                     # return skills to support skills
-                    xdotool mousemove 1843 1302; xdotool click 1
+                    toggle_top_skill
                     switch_modes 'midas' 'extract'
                     return
             fi
             : $((secs--))
         done
         # return skills to support skills
-        xdotool mousemove 1843 1302; xdotool click 1
+        toggle_top_skill
     done
 }
 
-echo -e "${ORANGE}Brandonds hilarious Midas Clicker!!!${NC}"
 
 main () {
+    echo -e "${ORANGE}Brandonds hilarious Midas Clicker!!!${NC}"
     focus_idle_skilling
     get_pid
     midas_clicks
