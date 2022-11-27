27b77a0993de92e122f6afc82229b000ad865617
brandon <brandongit>
2022-11-27 08:09:11 -0700

    Initial Python Commit.
    Using pynput:
    Implemented single and midas clicks in a thread
    Implemented hotkeys to pause or quit by listening for key combinations

---
+++ clickaroo/clicks.py
@@ -0,0 +1,82 @@
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import KeyCode, Key, GlobalHotKeys
+
+
+# click class
+class Clicker(threading.Thread):
+
+	def __init__(self, delay, button):
+		super(Clicker, self).__init__()
+		self.mouse = Controller()
+		self.delay = delay
+		self.button = button
+		self.running = False
+		self.program_running = True
+
+	def do_a_click(self, clicks):
+		click_count = 0
+		while click_count < clicks:
+			self.mouse.click(self.button)
+			clicks -= 1
+
+	def start_clicking(self):
+		self.running = True
+
+	def stop_clicking(self):
+		self.running = False
+
+	def exit(self):
+		self.stop_clicking()
+		self.program_running = False
+
+	# the click loop will run for 5 seconds unless stopped early.
+	def run(self):
+
+		while self.program_running:
+			end_time = time.time() + 5
+			while self.running:
+				if time.time() >= end_time:
+					self.running = False
+				self.mouse.click(self.button)
+				time.sleep(0.001)
+
+
+class InputMan:
+
+	def __init__(self, owner):
+		self.combination = {Key.ctrl, KeyCode(char='q')}
+		self.is_paused = False
+		self.listener = GlobalHotKeys({
+			'<ctrl>+<alt>+p': self.toggle_pause,
+			'<ctrl>+<alt>+q': self.exit_out
+		})
+		self.listener.start()
+		self.clickaroni = owner
+
+	def toggle_pause(self):
+		if not self.is_paused:
+			self.is_paused = True
+			self.clickaroni.click_thread.stop_clicking()
+			# pausing all other actions to be added later
+		else:
+			self.is_paused = False
+			# this will resume an action queue
+
+	def exit_out(self):
+		self.clickaroni.click_thread.exit()
+
+
+class Clickaroo:
+
+	def __init__(self):
+		self.click_thread = Clicker(delay=0.001, button=Button.left)
+		self.click_thread.start()
+
+		self.inputs = InputMan(self)
+
+
+if __name__ == '__main__':
+
+	clickaroni = Clickaroo()
---
+++ pydleskilling.py
@@ -0,0 +1,8 @@
+from clickaroo.clicks import Clickaroo
+
+
+if __name__ == '__main__':
+	# basic clicks and single click thread
+	# Can also press ctrl+alt+p to pause
+	#                ctrl+alt+q to quit
+	clickaroni = Clickaroo()
